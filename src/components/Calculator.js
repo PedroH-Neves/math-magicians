5b6af3e94a89863c71a3d35d727f5f698983634d
PedroH-Neves <PedroHDN710@gmail.com>
2023-07-08 21:23:06 -0400

    Add functionality to buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 import Display from './Display';
 
-const Calculator = () => (
-  <div id="calculator">
-    <Display />
-    <div id="calculator-items">
-      <button className="button" type="button">AC</button>
-      <button className="button" type="button">+/-</button>
-      <button className="button" type="button">%</button>
-      <button className="button orange" type="button">÷</button>
-      <button className="button" type="button">7</button>
-      <button className="button" type="button">8</button>
-      <button className="button" type="button">9</button>
-      <button className="button orange" type="button">x</button>
-      <button className="button" type="button">4</button>
-      <button className="button" type="button">5</button>
-      <button className="button" type="button">6</button>
-      <button className="button orange" type="button">-</button>
-      <button className="button" type="button">1</button>
-      <button className="button" type="button">2</button>
-      <button className="button" type="button">3</button>
-      <button className="button orange" type="button">+</button>
-      <button className="button" id="zero" type="button">0</button>
-      <button className="button" type="button">.</button>
-      <button className="button orange" type="button">=</button>
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (event) => {
+    const buttonName = event.target.innerText;
+    const result = calculate(state, buttonName);
+    setState(result);
+  };
+
+  const { total, next, operation } = state;
+
+  return (
+
+    <div id="calculator">
+      <Display
+        result={total}
+        next={next}
+        operation={operation}
+      />
+      <div id="calculator-items">
+        <button onClick={handleClick} className="button orange" type="button">AC</button>
+        <button onClick={handleClick} className="button orange" type="button">+/-</button>
+        <button onClick={handleClick} className="button orange" type="button">%</button>
+        <button onClick={handleClick} className="button orange" type="button">÷</button>
+        <button onClick={handleClick} className="button" type="button">7</button>
+        <button onClick={handleClick} className="button" type="button">8</button>
+        <button onClick={handleClick} className="button" type="button">9</button>
+        <button onClick={handleClick} className="button orange" type="button">x</button>
+        <button onClick={handleClick} className="button" type="button">4</button>
+        <button onClick={handleClick} className="button" type="button">5</button>
+        <button onClick={handleClick} className="button" type="button">6</button>
+        <button onClick={handleClick} className="button orange" type="button">-</button>
+        <button onClick={handleClick} className="button" type="button">1</button>
+        <button onClick={handleClick} className="button" type="button">2</button>
+        <button onClick={handleClick} className="button" type="button">3</button>
+        <button onClick={handleClick} className="button orange" type="button">+</button>
+        <button onClick={handleClick} className="button" id="zero" type="button">0</button>
+        <button onClick={handleClick} className="button" type="button">.</button>
+        <button onClick={handleClick} className="button orange" type="button">=</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
