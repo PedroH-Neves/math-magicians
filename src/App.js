08c01b40cd0c926cddd7a22817367bf9435a8929
PedroH-Neves <PedroHDN710@gmail.com>
2023-07-15 03:32:11 -0400

    Finish web site pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
 const App = () => (
-  <div>
-    <Calculator />
-    <Quote />
-  </div>
+  <Router>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Quote" element={<Quote />} />
+      <Route path="/Calculator" element={<Calculator />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
